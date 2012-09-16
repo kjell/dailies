be6d2ee5c25d763411601555d17ff5e35f684d13
Kjell <kjell@leanside.com>
2012-09-15 21:19:40 -0500

    Refresh ./bin/dailies.rb to my ~/bin/dailies.rb

--- bin/dailies.rb
+++ bin/dailies.rb
@@ -1,4 +1,4 @@
-#!/opt/local/bin/ruby
+#!/usr/bin/env ruby
 %w(rubygems fileutils simple-daemon).each {|lib| require lib}
 
 BASE = '/Users/Shared/dailies'
@@ -6,17 +6,24 @@ BASE = '/Users/Shared/dailies'
 class Dailies < SimpleDaemon::Base
   SimpleDaemon::WORKING_DIRECTORY = BASE
 
+  def self.snap
+    dir, file = File.join(BASE, Time.now.strftime('%Y/%m/%d-%H%M')).split('-')
+    FileUtils.mkdir_p(dir) unless File.directory?(dir)
+
+    #File.open("#{dir}/#{file}.log", 'w') do |f|
+    #  f <<
+    #end
+    system("/usr/local/bin/imagesnap #{dir}/#{file}.jpg")
+  end
+
   def self.start
     loop do
-      dir, file = File.join(BASE, Time.now.strftime('%Y/%m/%d')), Time.now.strftime('%H%M')
-      FileUtils.mkdir_p(dir) unless File.directory?(dir)
-
-      system("/Users/kjell/bin/camcapture #{dir}/#{file}")
-      sleep 1987
+      snap; sleep 1987
     end
   end
 
-  def self.stop; end
+  def self.stop
+  end
 end
 
-Dailies.daemonize
+ARGV[0] ? Dailies.daemonize : Dailies.snap
